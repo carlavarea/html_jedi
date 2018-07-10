3ebd6a90abdd2d48a29668f9225c661c22e81bb3
carlavarea <carla.varea@est.fib.upc.edu>
2018-07-10 11:16:38 +0200

    clock amb date

--- projecte/home.js
+++ projecte/home.js
@@ -3,13 +3,24 @@ function showTime(){
     var h = date.getHours(); // 0 - 23
     var m = date.getMinutes(); // 0 - 59
     var s = date.getSeconds(); // 0 - 59
+    var dayS = date.getDay();
+    var day = date.getDate();
+    var month = date.getMonth();
+    var year = date.getFullYear();
     
+    var monthA = ["Gener","Febrer","Març","Abril","Maig","Juny","Juliol","Agost","Septembre","Octubre","Novembre","Decembre"];
+    month = monthA[month];
+    var dayA =["Diumenge","Dilluns","Dimarts","Dimecres","Dijous","Divendres","Dissabte"];
+    dayS = dayA[dayS];
 
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
     
     var time = h + ":" + m + ":" + s;
+    var data = dayS + " " + day + " " + month + " " + year;
     document.getElementById("clock").innerText = time;
     document.getElementById("clock").textContent = time;
+    document.getElementById("date").innerText = data;
+    document.getElementById("date").textContent = data;
 }
