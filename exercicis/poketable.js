0dc67f44f349bf9707345f7953e49d33887c2617
carlavarea <carla.varea@est.fib.upc.edu>
2018-07-17 22:23:34 +0200

    poketable html done, falta buttons js

--- exercicis/poketable.js
+++ exercicis/poketable.js
@@ -1,12 +1,16 @@
 $().ready(function(){
-	$.get("http://40.118.8.76/pokemons/1",
+	$.get("http://40.118.8.76/pokemons/#id", {id: "6"},
 	function(data){
-		console.log(data);
-		$("#name").text(data.name);
-		$("#normal").attr('src' , data.images[0].front);
-		$("#shiny").attr('src' , data.images[1].front_shiny);
-		$("#code").text(data.num);
-		$("#type").text(data.types[0].type.name);
-		$("#hm").text(data.moves[0]);
+		console.log(data[0]);
+		$("#name").text(data[0].name);
+		$("#normal").attr('src' , data[0].images[0].front);
+		$("#shiny").attr('src' , data[0].images[1].front_shiny);
+		$("#code").text(data[0].num);
+		//mirem si té més d'1 type
+		if(data[0].types.length == 1) $("#type").text(data[0].types[0].type.name);
+		else $("#type").text(data[0].types[0].type.name + " & " + data[0].types[1].type.name);
+		//mirem si té hm o és null
+		if(data[0].moves.HM== null) $("#hm").text("None");
+		else $("#hm").text(data[0].moves.HM);
 	}, "json");
 });
